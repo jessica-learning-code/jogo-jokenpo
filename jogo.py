bfe7433cd055f4904004e4739122a6e1af7912cc
Jéssica Oliveira Carneiro <oliveiracarneirojessica@gmail.com>
2026-06-23 19:24:40 -0300

    .

--- jogo.py
+++ jogo.py
@@ -12,12 +12,16 @@ pygame.display.set_caption("JOGO JOKENPÔ")
 
 #fontes para as escrituras na tela
 fonte = pygame.font.SysFont("timesnewroman",30)
-fontee = pygame.font.SysFont("timesnewroman",20)
+fonte2 = pygame.font.SysFont("timesnewroman",25)
+
+
 
 #cores
 cor_branca = (255,255,255)
 cor_preta = (0,0,0)
 cor_verde = (0,255,0)
+cor_roxa = (128,0,255)
+cor_amarela = (0,128,128)
 
 #criar funções do jogo
 botao_pedra = pygame.image.load("pedra.png")
@@ -37,6 +41,8 @@ rect_tesoura = botao_tesoura.get_rect(topleft=(600,130))
 #criar loop finito
 fim_jogo = False
 imagem_computador = None
+
+#contador de pontos
 pontousuario = 0
 pontocomputador = 0
 pontoempate = 0
@@ -91,7 +97,7 @@ while not fim_jogo:
     
 
     #limpar a tela para desenhar o próximo frame(quadro)
-    tela.fill(cor_preta)
+    tela.fill(cor_amarela)
 
     #fazer a imagem imprimir na tela
     tela.blit(botao_pedra, rect_pedra)
@@ -99,30 +105,26 @@ while not fim_jogo:
     tela.blit(botao_tesoura, rect_tesoura)
 
     #Frases escritas na tela
-    texto = fonte.render("JOGO JOKENPÔ", True, cor_branca)
-    tela.blit(texto,(290,0))
-    texto2 = fontee.render("Escolha pedra, papel ou tesoura: " ,True, cor_branca)
-    tela.blit(texto2,(15,80))
+    texto = fonte.render("~~~~~~~~~~~~~~~~~~JOGO JOKENPÔ~~~~~~~~~~~~~~~~~~", True, cor_preta)
+    tela.blit(texto,(0,10))
+    texto2 = fonte2.render("Faça sua escolha ", True, cor_branca)
+    tela.blit(texto2,(30,80))
 
     if imagem_computador != None:
-        texto3 = fontee.render(f"Escolha do computador:", True, cor_branca)
+        texto3 = fonte2.render(f"Escolha do computador:", True, cor_branca)
         tela.blit(texto3,(15,220))
-        tela.blit(imagem_computador,(250,220))
+        tela.blit(imagem_computador,(270,220))
 
-    texto4 = fontee.render(f"Pontos COMPUTADOR: {pontocomputador}", True, cor_verde)
+    texto4 = fonte2.render(f"Pontos COMPUTADOR: {pontocomputador}", True, cor_verde)
     tela.blit(texto4,(15,300))
 
-    texto5 = fontee.render(f"Pontos USUÁRIO: {pontousuario}", True, cor_verde)
+    texto5 = fonte2.render(f"Pontos USUÁRIO: {pontousuario}", True, cor_verde)
     tela.blit(texto5,(15,350))
 
-    texto6 = fontee.render(f"Pontos EMPATE: {pontoempate}", True, cor_verde)
+    texto6 = fonte2.render(f"Pontos EMPATE: {pontoempate}", True, cor_verde)
     tela.blit(texto6,(15,400))
     
 
-        
-
     pygame.display.update()
-    
-
 pygame.quit()
 
